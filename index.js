2d2afabb6923534ffb9c46398dc69a97929fbaae
mitchelbade <mitchelm.bade@gmail.com>
2022-01-27 18:14:16 -0800

    commit tree 90a72cea7e5aadf9597c4b7dc2b180d55c5c2bdd

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Add your code here
+
+// function submitData() {
+//   fetch("http://localhost:3000/users", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     name: "Steve",
+//     email: "steve@steve.com",
+//   }),
+// })};
+
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+function submitData() {
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((res) => res.json())
+    .then((object) => addNewContact(object.id))
+
+    .catch((error) => document.body.innerHTML = 'Unauthorized Access')
+}
+
+function addNewContact(newID) {
+  document.body.innerHTML = newID;
+}
+
+
+
+submitData("name", "name@name.com")
+
